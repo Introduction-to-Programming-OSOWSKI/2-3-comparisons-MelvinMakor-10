b647a7e84751a24a7399af97dae2b8d9d3e9af08
MelvinMakor-10 <113386242+MelvinMakor-10@users.noreply.github.com>
2022-12-02 18:08:59 -0600

    1

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan():
+    1 = input("6 greater than 10")
+    if 1 == "No":
+        return "True"
+    else:
+        return "False"
+greaterThan()
+
+def equalTo():
+    2 = input("23 equal to 23")
+    if 2 == "Yes":
+        return "True"
+    else:
+        return "False"
+equalTo()
+
+def lessThan():
+    3 = input("2 less than 3")
+    if 3 == "Yes":
+        return "True"
+    else:
+        return "False"
+lessThan()
+
+def greaterOrEqual():
+    4 = input("17 greater or equal to 4")
+    if 4 == "Yes":
+        return "True"
+    else:
+        return "False"
+greaterOrEqual()
+
+def lessThanOrEqual():
+    5 = input("6 less than or equal to 4")
+    if 5 == "No":
+        return "True"
+    else:
+        "False"
